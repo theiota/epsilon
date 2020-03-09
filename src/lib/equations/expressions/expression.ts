e9d762961e9d17bcaac89f70a1471a774cb96eab
Rishi Kothari <itsrishikothari@gmail.com>
2020-03-08 21:03:09 -0400

    Update util for Expression

--- src/lib/equations/expressions/expression.ts
+++ src/lib/equations/expressions/expression.ts
@@ -66,17 +66,22 @@ export class Expression implements NormalExpression {
    * A command to add two expressions together. Supports function chaining.
    * @param expression The expression you wish to use the `add` command with.
    */
-  add(expression: Expression): Expression {
-    return expression
-  }
+    add(expression: Expression): Expression {
+        
+        return expression
+    }
 
-  /**
-   * A command to multiply two expressions together. Supports function chaining.
-   * @param expression The expression you wish to use the `multiply` command with.
-   */
-  multiply(expression: Expression): Expression {
-    return expression
-  }
+    /**
+     * A command to multiply two expressions together. Supports function chaining.
+     * @param expression The expression you wish to use the `multiply` command with.
+     */
+    multiply(expression: Expression): Expression {
+        let _expression: Expression = this
+
+
+
+        return expression
+    }
 
     /**
      * A command to add two expressions together.
@@ -89,152 +94,169 @@ export class Expression implements NormalExpression {
         return expression
     }
 
-  /**
-   * A command to subtract one expression from another. Supports function chaining.
-   * @param expression The expression you wish to use the `subtract` command with.
-   */
-  subtract(expression: Expression): Expression {
-    let _expression = expression
-    _expression = Expression.distributeOperators(expression, BasicOperators.Subtract)
+    /**
+     * A command to subtract one expression from another. Supports function chaining.
+     * @param expression The expression you wish to use the `subtract` command with.
+     */
+    subtract(expression: Expression): Expression {
+        let _expression = expression
+        _expression = Expression.distributeOperators(expression, Operators.Subtract)
 
-    console.log(Expression.appendExpressions(_expression, this))
+        console.log(Expression.appendExpressions(_expression, this))
 
-    return this
-  }
-
-  /**
-   * A helper function for [[distributeOperators]], in order to negate a certain type of operator.
-   * @param operator The operator to be negated.
-   */
-  public static negateDistributableOperator(
-    operator: DistributableOperator
-  ): DistributableOperator {
-    // VSCode will throw an error, stating that it's being used without being defined.
-    let returnOperator: DistributableOperator = BasicOperators.Subtract
-    switch (operator) {
-      case BasicOperators.Add:
-        returnOperator = BasicOperators.Subtract
-        break
-
-      case BasicOperators.Subtract:
-        returnOperator = BasicOperators.Add
-        break
-
-      default:
-        break
+        return this
     }
 
-    return returnOperator
-  }
+    /**
+     * A method to factor an [[Expression]].
+     *
+     * @returns {Expression}
+     * @memberof Expression
+     */
+    factor(): Expression {
+        return this
+    }
 
-  /**
-   * A function that distributes an operator over a certain expression.
-   * @param expression The expression to be distributed against
-   * @param operator The operator to distribute
-   */
-  static distributeOperators(expression: Expression, operator: DistributableOperator): Expression {
-    let prototypeExpression = expression
-    expression.terms.forEach((_, index) => {
-      switch (expression.operator) {
-        case BasicOperators.Add:
-          if (operator === BasicOperators.Add) {
-            expression.terms[index].operator = BasicOperators.Add
-          } else if (operator === BasicOperators.Subtract) {
-            expression.terms[index].operator = BasicOperators.Subtract
-          }
-          break
+    /**
+     * simplify
+     */
+    public simplify(): Expression {
+        return this
+    }
 
-        case BasicOperators.Subtract:
-          if (operator === BasicOperators.Add) {
-            expression.terms[index].operator = BasicOperators.Subtract
-          } else if (operator === BasicOperators.Subtract) {
-            expression.terms[index].operator = BasicOperators.Add
-          }
-          break
+    /**
+     * A helper function for [[distributeOperators]], in order to negate a certain type of operator.
+     * @param operator The operator to be negated.
+     */
+    public static negateDistributableOperator(
+        operator: DistributableOperator
+    ): DistributableOperator {
+        // VSCode will throw an error, stating that it's being used without being defined.
+        let returnOperator: DistributableOperator = Operators.Subtract
+        switch (operator) {
+            case Operators.Add:
+                returnOperator = Operators.Subtract
+                break
 
-        case BasicOperators.Multiply:
-          break
+            case Operators.Subtract:
+                returnOperator = Operators.Add
+                break
 
-        case BasicOperators.Divide:
-          break
-
-        default:
-          break
-      }
-    })
-    return prototypeExpression
-  }
-
-  /**
-   * A function used for concatenating two expressions. This is normally used a sa helper function for [collectLikeTerms].
-   * @param lhs The left hand side expression.
-   * @param rhs The right hand side expression.
-   */
-  public static appendExpressions(lhs: Expression, rhs: Expression): Expression {
-    let prototypeExpression: Expression = lhs
-
-    prototypeExpression.terms = [...lhs.terms, ...rhs.terms]
-    return prototypeExpression
-  }
-
-  public static collectLikeTerms(expression: Expression): Expression {
-    let prototypeExpression: Expression = expression
-    let termsWithoutCoefficients: any[] = []
-
-    expression.terms.forEach(term => {
-      termsWithoutCoefficients.push({
-        operator: term.operator,
-        value: term.value
-      })
-    })
-
-    let indicesOfLikeTerms: number[][] = []
-
-    termsWithoutCoefficients.forEach(element => {
-      let indices: number[] = []
-      termsWithoutCoefficients.forEach((termProt, index) => {
-        if (!([] as number[]).concat(...indicesOfLikeTerms).includes(index)) {
-          if (
-            termProt === element &&
-            element.operator !== BasicOperators.Multiply &&
-            element.operator !== BasicOperators.Divide
-          ) {
-            indices.push(index)
-          }
+            default:
+                break
         }
-      })
-      indicesOfLikeTerms.push(indices)
-    })
 
-    indicesOfLikeTerms.forEach(term => {
-      // TODO: loop through terms to sum up coefficients
-      console.log(term)
-    })
+        return returnOperator
+    }
 
-    return prototypeExpression
-  }
+    /**
+     * A function that distributes an operator over a certain expression.
+     * @param expression The expression to be distributed against
+     * @param operator The operator to distribute
+     */
+    static distributeOperators(expression: Expression, operator: DistributableOperator): Expression {
+        let prototypeExpression = expression
+        expression.terms.forEach((_, index) => {
+            switch (expression.operator) {
+                case Operators.Add:
+                    if (operator === Operators.Add) {
+                        expression.terms[index].operator = Operators.Add
+                    } else if (operator === Operators.Subtract) {
+                        expression.terms[index].operator = Operators.Subtract
+                    }
+                    break
 
-  /**
-   * The method to stringify an expression using its `this` property.
-   */
-  public toString(): String {
-    return Expression.toString(this)
-  }
+                case Operators.Subtract:
+                    if (operator === Operators.Add) {
+                        expression.terms[index].operator = Operators.Subtract
+                    } else if (operator === Operators.Subtract) {
+                        expression.terms[index].operator = Operators.Add
+                    }
+                    break
 
-  /**
-   * The general method for stringifying an Expression.
-   * @param expression The expression to be stringified
-   */
-  public static toString(expression: Expression): String {
-    let prototypeString: string = ''
+                case Operators.Multiply:
+                    break
 
-    expression.terms.forEach(term => {
-      prototypeString += term.toString()
-    })
+                case Operators.Divide:
+                    break
 
-    prototypeString = `${expression.operator}(${prototypeString})`
+                default:
+                    break
+            }
+        })
+        return prototypeExpression
+    }
 
-    return prototypeString
-  }
-    
+    /**
+     * A function used for concatenating two expressions. This is normally used a sa helper function for [collectLikeTerms].
+     * @param lhs The left hand side expression.
+     * @param rhs The right hand side expression.
+     */
+    public static appendExpressions(lhs: Expression, rhs: Expression): Expression {
+        let prototypeExpression: Expression = lhs
+
+        prototypeExpression.terms = [...lhs.terms, ...rhs.terms]
+        return prototypeExpression
+    }
+
+    public static collectLikeTerms(expression: Expression): Expression {
+        let prototypeExpression: Expression = expression
+        let termsWithoutCoefficients: any[] = []
+
+        expression.terms.forEach(term => {
+            termsWithoutCoefficients.push({
+                operator: term.operator,
+                value: term.value
+            })
+        })
+
+        let indicesOfLikeTerms: number[][] = []
+
+        termsWithoutCoefficients.forEach(element => {
+            let indices: number[] = []
+            termsWithoutCoefficients.forEach((termProt, index) => {
+                if (!([] as number[]).concat(...indicesOfLikeTerms).includes(index)) {
+                    if (
+                        termProt === element &&
+                        element.operator !== Operators.Multiply &&
+                        element.operator !== Operators.Divide
+                    ) {
+                        indices.push(index)
+                    }
+                }
+            })
+            indicesOfLikeTerms.push(indices)
+        })
+
+        indicesOfLikeTerms.forEach(term => {
+            // TODO: loop through terms to sum up coefficients
+            console.log(term)
+        })
+
+        return prototypeExpression
+    }
+
+    /**
+     * The method to stringify an expression using its `this` property.
+     */
+    public toString(): String {
+        return Expression.toString(this)
+    }
+
+    /**
+     * The general method for stringifying an Expression.
+     * @param expression The expression to be stringified
+     */
+    public static toString(expression: Expression): String {
+        let prototypeString: string = ''
+
+        expression.terms.forEach(term => {
+            prototypeString += term.toString()
+        })
+
+        prototypeString = `${expression.operator}(${prototypeString})`
+
+        return prototypeString
+    }
+
 }
