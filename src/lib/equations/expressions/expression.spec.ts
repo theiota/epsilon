e54720dd9d46540ed3e16f17cee605d20774d42b
Rishi Kothari <itsrishikothari@gmail.com>
2020-03-13 10:25:44 -0400

    Updated Operator name

--- src/lib/equations/expressions/expression.spec.ts
+++ src/lib/equations/expressions/expression.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 import { Expression } from './expression';
 import { Constant } from '../../symbols/constants/constant';
-import { BasicOperators } from '../../symbols/operators/operators';
+import { Operators } from '../../symbols/operators/operators';
 import { Term } from '../../symbols/terms/term';
 import { Variable } from '../../symbols/variables/variable';
