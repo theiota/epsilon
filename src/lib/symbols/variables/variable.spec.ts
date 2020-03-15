f7302c7fcfc43bdc9c88bd158a7194be8bde2886
Rishi Kothari <itsrishikothari@gmail.com>
2020-03-15 12:27:05 -0400

    ✅ Add tests for Variables

--- src/lib/symbols/variables/variable.spec.ts
+++ src/lib/symbols/variables/variable.spec.ts
@@ -4,6 +4,7 @@ import { Constant } from '../constants/constant';
 import { GreekLetter } from '../types/GreekLetter';
 import { Term } from '../terms/term';
 import { Operators } from '../operators/operators';
+import { Expression } from '../../equations/expressions/expression';
 
 /*
 
@@ -30,6 +31,7 @@ Utility tests
 /* Functions used in  */
 
 /* Operators */
+
 
 /* Addition */
 test("Can add two variables of the same name", t => {
@@ -58,4 +60,58 @@ test("Can add two variables of the same name", t => {
     t.deepEqual(addition, expected)
 })
 
+test("Can add two different variables", t => {
+    let x = new Variable({
+        name: "x",
+        exponent: new Constant({
+            value: 2
+        })
+    })
+
+    let y = new Variable({
+        name: "y",
+        exponent: new Constant({
+            value: 2
+        })
+    })
+
+    let addition = x.add(y)
+
+    let expected = new Expression({
+        terms: [
+            x.toTerm(),
+            y.toTerm()
+        ],
+        operator: Operators.Add
+    })
+
+    t.deepEqual(addition, expected)
+})
+
+test("Can add a variable and a constant", t => {
+    let x = new Variable({
+        name: "x",
+        exponent: new Constant({
+            value: 2
+        })
+    })
+
+    let constant = new Constant({
+        value: 3
+    })
+
+    let addition = x.add(constant)
+
+    let expected = new Expression({
+        terms: [
+            x.toTerm(),
+            constant.toTerm()
+        ],
+        operator: Operators.Add
+    })
+
+    t.deepEqual(addition, expected)
+})
+
+test.todo("Tests for function chaining in addition")
 // TODO: Finish off tests for variables
